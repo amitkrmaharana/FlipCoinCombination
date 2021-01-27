ae79e4112ba02d21e16b010eae006395a6c856b6
amitkrmaharana <ak657harley@gmail.com>
2021-01-27 11:01:05 +0530

    Tossed the coin

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,3 +1,11 @@
 #!/bin/bash -x
+isHead=0
+isTail=1
 echo "Welcome to Flip Coin Combination Simulator Program"
-
+flipCoin=$((RANDOM%2))
+if [ $flipCoin -eq $isHead ]
+then
+	echo "HEADS"
+else
+	echo "TAILS"
+fi
