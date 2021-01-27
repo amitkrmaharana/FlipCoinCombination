365ebadce8fbd30c22b56b62504ba784aff8a4d1
amitkrmaharana <ak657harley@gmail.com>
2021-01-27 11:12:10 +0530

    Made 20 tosses and determined percentage of heads and tails

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,11 +1,22 @@
-#!/bin/bash -x
+#!/bin/bash
+declare -A singlet
 isHead=0
 isTail=1
+head_count=0
+tail_count=0
 echo "Welcome to Flip Coin Combination Simulator Program"
-flipCoin=$((RANDOM%2))
-if [ $flipCoin -eq $isHead ]
-then
-	echo "HEADS"
-else
-	echo "TAILS"
-fi
+for((i=1;i<=20;i++))
+do
+	flipCoin=$((RANDOM%2))
+	case $flipCoin in
+	$isHead)singlet["flip$i"]="HEADS"
+		(( head_count++ ));;
+	$isTail)singlet["flip$i"]="TAILS"
+		(( tail_count++ ))
+	esac
+done
+head_percent=$((head_count*100/20))
+tail_percent=$((tail_count*100/20))
+echo ${singlet[@]}
+echo ${!singlet[@]}
+echo "Percentage of heads is $head_percent and that of tails is $tail_percent"
