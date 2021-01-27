4f852faa33c392d7f84be3338f33331cbf0fdab7
amitkrmaharana <ak657harley@gmail.com>
2021-01-27 11:55:40 +0530

    Triplet combination initialised

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,40 +1,72 @@
 #!/bin/bash
-declare -A doublet
+declare -A triplet
 isHead=0
 isTail=1
-hh_count=0
-ht_count=0
-th_count=0
-tt_count=0
+hhh_count=0
+hht_count=0
+hth_count=0
+htt_count=0
+thh_count=0
+tht_count=0
+tth_count=0
+ttt_count=0
 echo "Welcome to Flip Coin Combination Simulator Program"
 for((i=1;i<=20;i++))
 do
 	flipCoin1=$((RANDOM%2))
 	flipCoin2=$((RANDOM%2))
+	flipCoin3=$((RANDOM%2))
 	case $flipCoin1 in
 		$isHead)case $flipCoin2 in
-				$isHead)doublet["flip$i"]="HH"
-					(( hh_count++ ));;
-				$isTail)doublet["flip$i"]="HT"
-					(( ht_count++ ));;
+				$isHead)case $flipCoin3 in
+						$isHead)triplet["flip$i"]="HHH"
+							(( hhh_count++ ));;
+						$isTail)triplet["flip$i"]="HHT"
+							(( hht_count++ ));;
+					esac;;
+				$isTail)case $flipCoin3 in
+                                                $isHead)triplet["flip$i"]="HTH"
+                                                        (( hth_count++ ));;
+                                                $isTail)triplet["flip$i"]="HTT"
+                                                        (( htt_count++ ));;
+                                        esac;;
+
 			esac;;
 		$isTail)case $flipCoin2 in
-                        	$isHead)doublet["flip$i"]="TH"
-                        	        (( th_count++ ));;
-                        	$isTail)doublet["flip$i"]="TT"
-                        	        (( tt_count++ ));;
+                        	$isHead)case $flipCoin3 in
+                                                $isHead)triplet["flip$i"]="THH"
+                                                        (( thh_count++ ));;
+                                                $isTail)triplet["flip$i"]="THT"
+                                                        (( tht_count++ ));;
+                                        esac;;
+
+                        	$isTail)case $flipCoin3 in
+                                                $isHead)triplet["flip$i"]="TTH"
+                                                        (( tth_count++ ));;
+                                                $isTail)triplet["flip$i"]="TTT"
+                                                        (( ttt_count++ ));;
+                                        esac;;
+
                 	esac;;
 
 	esac
 done
-hh_percent=$((hh_count*100/20))
-ht_percent=$((ht_count*100/20))
-th_percent=$((th_count*100/20))
-tt_percent=$((tt_count*100/20))
-echo ${doublet[@]}
-echo ${!doublet[@]}
-echo "Percentage of hh is $hh_percent"
-echo "Percentage of ht is $ht_percent"
-echo "Percentage of th is $th_percent"
-echo "Percentage of tt is $tt_percent"
+hhh_percent=$((hhh_count*100/20))
+hht_percent=$((hht_count*100/20))
+hth_percent=$((hth_count*100/20))
+htt_percent=$((htt_count*100/20))
+thh_percent=$((thh_count*100/20))
+tht_percent=$((tht_count*100/20))
+tth_percent=$((tth_count*100/20))
+ttt_percent=$((ttt_count*100/20))
+echo ${triplet[@]}
+echo ${!triplet[@]}
+echo "Percentage of hhh is $hhh_percent"
+echo "Percentage of hht is $hht_percent"
+echo "Percentage of hth is $hth_percent"
+echo "Percentage of htt is $htt_percent"
+echo "Percentage of thh is $thh_percent"
+echo "Percentage of tht is $tht_percent"
+echo "Percentage of tth is $tth_percent"
+echo "Percentage of ttt is $ttt_percent"
 
